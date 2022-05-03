99dafede7f2b83d7c0271dc29f8c2c00703f30d1
Jonathan Ruiz <jonathan.ruizz@hotmail.com>
2022-05-03 11:47:23 -0400

    Se eliminó los comentarios de app.ja, además evitamos crear un custom hook, ya que, no estamos usando refencias pero, queda como tarea

--- src/App.js
+++ src/App.js
@@ -23,57 +23,12 @@ const App = () => {
   }
 
   return (
-      <Form submit={ submit }>
-{/*         <div className="ed-item s-center">
-          <img 
-            className="s-mb-2" 
-            src={logo} 
-            alt="logo la gotera"
-          />
-        </div> */}
-
-{/*         <div className="ed-item form__item">
-          <label htmlFor="email">
-            Correo electrónico
-            <input 
-              type="email" 
-              name="email" 
-              id="email" 
-              value={data.email} 
-              onChange={changeData}
-            />
-          </label>
-        </div> */}
-
-{/*         <div className="ed-item form__item">
-          <label htmlFor="password">
-            Contraseña
-            <input 
-              type="password" 
-              name="password" 
-              id="password"
-              value={data.password}
-              onChange={changeData}
-            />
-          </label>
-        </div> */}
-
-        <Logo logoImg={ logo } alternative="Logo login" />
-        <Field type="email" value={data.email} label="Correo electrónico" change={ changeData } />
-        <Field type="password" value={data.password} label="Contraseña" change={ changeData } />
-
-
-{/*     <div className="ed-item form__item">
-          <input 
-            type="submit" 
-            value="Ingresar" 
-            className="button full"
-          />
-        </div> */}
-
-        <Submit value="Ingresar"/>
-
-      </Form>
+    <Form submit={ submit }>
+      <Logo logoImg={ logo } alternative="Logo login" />
+      <Field type="email" value={data.email} label="Correo electrónico" change={ changeData } />
+      <Field type="password" value={data.password} label="Contraseña" change={ changeData } />
+      <Submit value="Ingresar"/>
+    </Form>
   );
 }
 
